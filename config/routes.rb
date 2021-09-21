7a0c04b33f723ce2b86453e0781a6de907aa313a
Lawrence <lchua2314@gmail.com>
2021-09-20 22:00:27 -0700

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root "assignments#index"
+  root "assignments#index"
 
   get "/assignments", to: "assignments#index"
 
