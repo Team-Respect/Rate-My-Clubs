b4af5ccdc1e141ede7b358f55c6884a0b71b7b34
AllenNguyen0417 <thanhquan1704@gmail.com>
2021-10-01 23:15:35 -0700

    SignUp/LogIn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :validatable
   has_many :ratings
   has_many :clubs # Just to prevent trolling, might not implement
 
