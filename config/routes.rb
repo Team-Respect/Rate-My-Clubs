7101defddbd847c78c84f749b19bc8f6c2f9e949
Lawrence <lchua2314@gmail.com>
2021-10-07 15:13:31 -0700

    Edited routes formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,19 @@
 Rails.application.routes.draw do
-  resources :ratings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # Home page route
-
   root "clubs#index"
 
   # About page route
-
   get '/about', to: "about#index"
 
   # Sign Up page route
-
   get '/sign_up', to: "sign_up#index"
 
   # Sign Up page route
-
   get '/login', to: "login#index"
 
   # Forgot Password page route
-
   get '/password/forgot', to: "password#index"
 
   # User authentication
@@ -36,34 +30,24 @@ Rails.application.routes.draw do
   resources :categories
 
   # Ratings
+  resources :ratings
 
   # We are not using the below anymore
   # Assignments route
-
   get "/assignments", to: "assignments#index"
 
   # Add A3 work below
-
   get "/assignments/a3Lawrence", to: "assignments#a3Lawrence"
-  
   get "/assignments/a3ThanhQuan", to: "assignments#a3ThanhQuan"
-
   get "/assignments/a3Minkyu", to: "assignments#a3Minkyu"
-
   get "/assignments/a3Max", to: "assignments#a3Max"
-
   get "/assignments/a3Cynthia", to: "assignments#a3Cynthia"
 
   # Add A4 work below
-
   get "/assignments/a4Lawrence", to: "assignments#a4Lawrence"
-  
   get "/assignments/a4ThanhQuan", to: "assignments#a4ThanhQuan"
-  
   get "/assignments/a4Max", to: "assignments#a4Max"
-
   get "/assignments/a4Minkyu", to: "assignments#a4Minkyu"
-
   get "/assignments/a4Cynthia", to: "assignments#a4Cynthia"
 
   namespace :assignments do
