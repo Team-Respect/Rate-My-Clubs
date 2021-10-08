2ee4398d352c6bb39194eed35e97192f3aa800d2
AllenNguyen0417 <thanhquan1704@gmail.com>
2021-10-08 13:10:25 -0700

    Update usermanagement to users

--- db/migrate/20210918084226_create_assignments_a4_thanh_quan_tests.rb
+++
@@ -1,9 +0,0 @@
-class CreateAssignmentsA4ThanhQuanTests < ActiveRecord::Migration[6.1]
-  def change
-    create_table :assignments_a4_thanh_quan_tests do |t|
-      t.string :image
-
-      t.timestamps
-    end
-  end
-end
