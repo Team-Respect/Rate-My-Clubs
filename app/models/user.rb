26e85df31b995a64dbe1b516621964c6202050b7
AllenNguyen0417 <thanhquan1704@gmail.com>
2021-10-08 11:45:16 -0700

    Revert "Update usermanagement to user"
    This reverts commit a6d315e98fed16f7190da9c73203ee90a1a826d6.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
-
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :validatable
+  
+  #has_many :ratings
+  
+  # Add validations here
+  # Ex: uniqueness for email/username?
+  # length of passsword?
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get '/password/forgot', to: "password#index"
 
   # User authentication
+  devise_for :usermanagements
   devise_for :users
 
   # Clubs and Ratings
