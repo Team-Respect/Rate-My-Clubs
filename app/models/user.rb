fbbe606422a48f77af9ed5e0d6e4893bf717bb05
AllenNguyen0417 <thanhquan1704@gmail.com>
2021-10-08 14:06:41 -0700

    Update usermanagement to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #       :recoverable, :rememberable, :validatable
-  
-  #has_many :ratings
-  
-  # Add validations here
-  # Ex: uniqueness for email/username?
-  # length of passsword?
-  
+         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
+
 end
