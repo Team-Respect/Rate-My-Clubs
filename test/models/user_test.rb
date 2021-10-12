9661290333befd50c9ca316a9bfde3357b9cd041
Saiph0 <cynthialuong403@gmail.com>
2021-10-12 13:33:32 -0700

    Added unit test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "cannot create user without a name" do
+    user = User.new
+    assert_not user.save, "blank user saved"
+  end
 end
