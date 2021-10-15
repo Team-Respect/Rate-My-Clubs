0242571facbfaeed34f19eb864ec8f97e5cc1089
AllenNguyen0417 <thanhquan1704@gmail.com>
2021-10-15 16:48:44 -0700

    Created User DropDown

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,16 @@
 $("div[data-link]").on( "click", function() {
   window.location = $(this).data("link");
 });
+
+
+// $('.user_icon').hover(function() {
+//     $(this).find('li.user_items').show(200);
+// }, function(){
+//     $(this).find('li.user_items').hide(400);
+// });
+
+$(document).on('click', '.user_icon', function (e) {
+  $(this).find('li.user_items').toggle(200);
+});
+
+
