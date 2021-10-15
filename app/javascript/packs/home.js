4efa946242f7a92b873ed240ae8629baa89c4446
Quan Nguyen <71422583+AllenNguyen0417@users.noreply.github.com>
2021-10-15 16:49:51 -0700

    Update home.js

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -3,12 +3,6 @@ $("div[data-link]").on( "click", function() {
 });
 
 
-// $('.user_icon').hover(function() {
-//     $(this).find('li.user_items').show(200);
-// }, function(){
-//     $(this).find('li.user_items').hide(400);
-// });
-
 $(document).on('click', '.user_icon', function (e) {
   $(this).find('li.user_items').toggle(200);
 });
