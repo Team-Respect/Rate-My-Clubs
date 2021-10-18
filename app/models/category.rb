b5c75eee044bd79916943b9f65273c634148b45a
mkyuyang <46985141+mkyuyang@users.noreply.github.com>
2021-10-17 19:11:32 -0700

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,11 @@
 class Category < ApplicationRecord
   # Validate that the category name is not empty.
-      # Validate that the category name is not duplicated, or less than 5 characters.
-      validates :name, presence: true, uniqueness: true, length: { minimum: 5 }
+    # Validate that the category name is not duplicated, or less than 5 characters.
+    validates :name, presence: true, uniqueness: true, length: { minimum: 5 }
   
   # Validate that the category description is not empty.
-      # Validate that the category description not duplicated, or less than 5 characters.
-      validates :description, presence: true, uniqueness: true, length: { minimum: 5 }
+    # Validate that the category description not duplicated, or less than 5 characters.
+    validates :description, presence: true, uniqueness: true, length: { minimum: 5 }
   
   has_many :clubs
 end
