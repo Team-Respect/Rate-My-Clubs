1ab0819b3060d9f8e92e0bb60d9b290a7a4ecc72
AllenNguyen0417 <thanhquan1704@gmail.com>
2021-10-17 23:01:34 -0700

    Implemented Mailer Asking User To Confirm Email After Signing Up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,8 +75,18 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "thanhquan1704@gmail.com",
+    password: "bkgqcksohbmgsoxx",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
 end
