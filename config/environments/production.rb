7cfd7a1e389274aee89974287eb1be4b4b2e3531
Lawrence <lchua2314@gmail.com>
2021-10-21 18:24:36 -0700

    Changed production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,19 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'lit-savannah-91482.herokuapp', port: 3000 }
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "ratemyclubs2021@gmail.com",
+    password: "bfcfgwksxayahior",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
 end
