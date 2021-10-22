8ade12dd661119f5a3a8716c99fc8098cb0f0ace
Lawrence <lchua2314@gmail.com>
2021-10-21 18:42:44 -0700

    Changed production mailer port number 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'lit-savannah-91482.herokuapp', port: 80 }
+  config.action_mailer.default_url_options = { host: 'lit-savannah-91482.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
