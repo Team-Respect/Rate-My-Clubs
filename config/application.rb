5dc7d5725f3b23af20adf82fcd30f4d12fdf1a32
Lawrence <lchua2314@gmail.com>
2021-10-28 17:54:36 -0700

    Fixed incorrect rating dates

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RateMyClubs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Pacific Time (US & Canada)'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
