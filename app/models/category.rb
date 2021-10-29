870f9a3467073d7db190a5b4362471423992f807
Lawrence <lchua2314@gmail.com>
2021-10-28 17:18:45 -0700

    Added curse validations to Clubs, Ratings, and Categories using Obscenity Gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,11 @@
 class Category < ApplicationRecord
   # Validate that the category name is not empty.
   # Validate that the category name is not duplicated, or less than 5 characters.
-  validates :name, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 5 }, obscenity: true
   
   # Validate that the category description is not empty.
   # Validate that the category description not duplicated, or less than 5 characters.
-  validates :description, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :description, presence: true, uniqueness: true, length: { minimum: 5 }, obscenity: true
   
   has_many :clubs, :dependent => :destroy
 end
